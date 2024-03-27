5b24bb150e58a66f77eb6da0ded80244d90b12b4
kent970 <jakubkedzierski970@gmail.com>
2024-03-27 18:30:58 +0100

    mediportaTask initial

---
+++ SOApiProject/Controllers/SOController.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using SOApiProject.Models;
+
+namespace SOApiProject.Controllers;
+
+[ApiController]
+[Route("api/so")]
+public class SOController : ControllerBase
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IConfiguration _configuration;
+
+    public SOController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public async Task<Response> GetTags(int pageNum = 1, int pageSize = 100)
+    {
+        var httpClient = _httpClientFactory.CreateClient("SOClient");
+
+
+        string apiBaseUrl = _configuration["ApiSettings:BaseUrl"];
+
+        string apiUrl = $"{apiBaseUrl}?page={pageNum}&pagesize={pageSize}&order=desc&sort=popular&site=stackoverflow";
+
+
+        var response = await httpClient.GetAsync(apiUrl);
+
+        response.EnsureSuccessStatusCode();
+
+        if (response.IsSuccessStatusCode)
+        {
+            var contentStream = await response.Content.ReadAsStreamAsync();
+            using (var gzip = new System.IO.Compression.GZipStream(contentStream,
+                       System.IO.Compression.CompressionMode.Decompress))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                var result = JsonSerializer.Deserialize<Response>(gzip, options);
+
+                return result;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ SOApiProject/Models/TagModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SOApiProject.Models;
+public class TagModel
+{
+    public bool HasSynonyms { get; set; }
+    public bool IsModeratorOnly { get; set; }
+    public bool IsRequired { get; set; }
+    public int Count { get; set; }
+    public string Name { get; set; }
+}
+public class Response
+{
+    public List<TagModel> Items { get; set; }
+}
